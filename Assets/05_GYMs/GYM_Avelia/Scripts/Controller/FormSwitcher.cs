91fe9866ac4512775fc7ea144980a5fd7e5a0874
Bard Avelia <a.bard@rubika-edu.com>
2026-02-10 16:06:04 -0500

    first draft of the form swicther script

--- Assets/05_GYMs/GYM_Avelia/Scripts/Controller/FormSwitcher.cs
+++ Assets/05_GYMs/GYM_Avelia/Scripts/Controller/FormSwitcher.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class FormSwitcher : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Form currentForm { get; private set; } = Form.neutral;
+
+    [SerializeField]
+    GameObject neutralFormObject, dreamFormObject, nightmareFormObject;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeForm(Form nextForm)
     {
-        
+        neutralFormObject.SetActive(false);
+        dreamFormObject.SetActive(false);
+        nightmareFormObject.SetActive(false);
+
+        switch (nextForm)
+        {
+            case Form.neutral:
+                neutralFormObject.SetActive(true);
+                break;
+            case Form.dream:
+                dreamFormObject.SetActive(true);
+                break;
+            case Form.nightmare:
+                nightmareFormObject.SetActive(true);
+                break;
+        }
+
     }
+}
+
+public enum Form
+{
+    neutral,
+    dream,
+    nightmare,
 }
