e0c2b8a862ebe06bf4fbdccad737b4fdcf22fd3a
Bard Avelia <a.bard@rubika-edu.com>
2026-02-10 14:35:45 -0500

    Script to implement mana drain/gain

---
+++ Assets/05_GYMs/GYM_Avelia/Scripts/ManaDrainer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class ManaDrainer : MonoBehaviour
+{
+    [SerializeField]
+    float ManaToGain = 0, numberOfSecondBetweenTicks = 1;
+
+    void OnEnable()
+    {
+        _ = StartCoroutine(DrainMana());
+    }
+
+    IEnumerator DrainMana()
+    {
+        while (true)
+        {
+            print($"lost {ManaToGain} mana TO IMPLEMENT");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
