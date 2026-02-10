5ca7b134a9db5eea91b3bcafd4f62b4bbdd47fa6
Bard Avelia <a.bard@rubika-edu.com>
2026-02-10 13:20:05 -0500

    added rough first draft (NOT FINAL AT ALL STILL CHANGING)

--- Assets/05_GYMs/GYM_Avelia/Scripts/Controller/PlayerController.cs
+++ Assets/05_GYMs/GYM_Avelia/Scripts/Controller/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    CharacterController controller;
+
+    [SerializeField]
+    float speed = 10f;
+
+    Vector2 direction;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new(direction.x, -1f, direction.y);
+
+        controller.Move(movement * speed * Time.deltaTime);
     }
+
+    void OnMove(InputValue _input)
+    {
+        direction = _input.Get<Vector2>();
+        print(direction);
+
+        if (direction.magnitude == 0) return;
+
+        var angle = Mathf.Atan2(direction.y, direction.x);
+        angle = Mathf.Rad2Deg * angle;
+
+        transform.rotation = Quaternion.Euler(0, -angle, 0);
+    }
+
 }
