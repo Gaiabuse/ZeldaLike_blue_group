23a86a8ed08a85d15bce43f0f5fc3e284dc874fd
DeltaUniqx <uniqxdeslandes@gmail.com>
2026-02-23 13:49:01 -0500

    Player Wall test

--- Assets/05_GYMs/GYM_Antoine/Antoine Folder/Script/Player_Test.cs
+++ Assets/05_GYMs/GYM_Antoine/Antoine Folder/Script/Player_Test.cs
@@ -4,11 +4,26 @@ public class Player_Test : MonoBehaviour
 {
     Rigidbody rb;
 
+    [Header("Movement")]
+    [SerializeField] Transform CammeraPose;
     [SerializeField] float speed;
+    [SerializeField] float speedRotate;
+
+    [SerializeField] float jumpPower = 25f;
+
+    [Header("Gravity")]
+    [SerializeField] Transform GroundCheckPose;
+    [SerializeField] float GroundCheckRadius = 0.1f;
+    [SerializeField] bool isGrounded;
+
+    [SerializeField] bool isOnWall;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = true;
+        isOnWall = false;
     }
 
     private void Update()
@@ -16,6 +31,75 @@ public class Player_Test : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rb.linearVelocity = new Vector3(horizontal * speed, rb.linearVelocity.y, vertical * speed);
+        if (!isOnWall)
+        {
+            rb.linearVelocity = new Vector3(horizontal * speed, rb.linearVelocity.y, vertical * speed);
+        }
+        else
+        {
+            rb.linearVelocity = transform.right * horizontal * speed + transform.forward * vertical * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOnWall)
+            {
+                rb.AddForce(transform.up * jumpPower * 10, ForceMode.Impulse);
+
+                transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                isOnWall = false;
+                rb.useGravity = true;
+            }
+            else
+            {
+                rb.linearVelocity = Vector3.zero;
+                rb.linearVelocity += transform.up * jumpPower;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = GroundCheck();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            isOnWall = !isOnWall;
+
+            if (isOnWall)
+            {
+                transform.rotation = Quaternion.Euler(-90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                rb.useGravity = false;
+
+                RaycastHit hit;
+                if (Physics.Raycast(GroundCheckPose.position, -transform.up, out hit, 1f))
+                {
+
+                }
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                rb.useGravity = true;
+            }
+        }
+    }
+
+    bool GroundCheck()
+    {
+        Collider[] hitGroundLayer = Physics.OverlapSphere(GroundCheckPose.position, GroundCheckRadius);
+
+        if (hitGroundLayer.Length > 0)
+        {
+            foreach (Collider col in hitGroundLayer)
+            {
+                if (col.gameObject.CompareTag("Ground")) return true;
+            }
+        }
+
+        return false;
     }
 }
