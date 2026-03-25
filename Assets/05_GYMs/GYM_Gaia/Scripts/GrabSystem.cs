b8a44afeed751d92ccaeb5f69d1760ebda6a5e9d
Bard Avelia <a.bard@rubika-edu.com>
2026-03-25 19:27:00 -0400

    added grabbing mark

--- Assets/05_GYMs/GYM_Gaia/Scripts/GrabSystem.cs
+++ Assets/05_GYMs/GYM_Gaia/Scripts/GrabSystem.cs
@@ -5,19 +5,26 @@ using UnityEngine.InputSystem;
 
 public class GrabSystem : MonoBehaviour
 {
+    [Header("Do not change")]
     [SerializeField]
     PlayerController player;
+
+    [Header("Grab")]
     [SerializeField] private float rangeForGrab;
     [SerializeField] private float grabStrength;
     [SerializeField] private float rangeForSwallow;
     [SerializeField] private LayerMask grabLayers;
     [SerializeField] private Vector3 downValue = Vector3.down;
 
+    [Header("Throw")]
+    [Tooltip("The enemy will end up at this distance of the enemy")]
     [SerializeField] private float throwDistance;
-    [SerializeField] private float throwDuration;
+    [Tooltip("Duration in seconds")]
+    [SerializeField] private float throwDuration, AutoThrowDuration;
 
+    [Header("Visual")]
     [SerializeField]
-    private GameObject throwMark;
+    private GameObject throwMark, grabMark;
 
     private GameObject currentGrabbedObject;
 
@@ -33,6 +40,8 @@ public class GrabSystem : MonoBehaviour
 
     private GrabbingState grabbingState;
 
+    private float throwTimer;
+
     void Start()
     {
         throwMark.transform.localPosition = Vector3.forward * throwDistance;
@@ -45,12 +54,12 @@ public class GrabSystem : MonoBehaviour
             case GrabbingState.None:
                 break;
             case GrabbingState.ShowGrabPred:
-                break;
-            case GrabbingState.ShowThrowPred:
                 ShowGrabPredictionUpdate();
                 break;
+            case GrabbingState.ShowThrowPred:
+                break;
             case GrabbingState.TimerLimitThrow:
-                throw new System.NotImplementedException($"{nameof(Update)} {nameof(GrabSystem)} not finished to be implemented");
+                break;
 
         }
 
@@ -94,10 +103,14 @@ public class GrabSystem : MonoBehaviour
 
         Grab();
         player.CanMove = true;
+        throwTimer = Time.deltaTime;
     }
 
     private void Throw()
     {
+        grabbingState = GrabbingState.None;
+        grabMark.SetActive(false);
+
         IsThrowing = true;
         currentGrabbedObject.SetActive(true);
 
@@ -141,10 +154,28 @@ public class GrabSystem : MonoBehaviour
 
     private void ShowGrabPredictionUpdate()
     {
+        grabMark.SetActive(true);
+
         Vector3 downPosition = transform.position - downValue;
+
         if (Physics.Raycast(downPosition, transform.forward, out RaycastHit hitSwallow, rangeForSwallow, grabLayers))
         {
+            PutGrabMarkAtTarget(hitSwallow.collider.transform.position);
+            return;
         }
+
+        if (Physics.Raycast(downPosition, transform.forward, out RaycastHit hitGrabbed, rangeForGrab, grabLayers))
+        {
+            PutGrabMarkAtTarget(hitGrabbed.collider.transform.position);
+            return;
+        }
+
+        grabMark.SetActive(false);
+    }
+
+    private void PutGrabMarkAtTarget(Vector3 position)
+    {
+        grabMark.transform.position = position;
     }
 
     private void Grab()
@@ -177,11 +208,11 @@ public class GrabSystem : MonoBehaviour
 
         if (hitGrabbed.collider.transform.parent != null)
         {
-            grabbedObject = hitGrabbed.collider.transform.parent.GetComponent<Rigidbody>();
+            grabbedObject = hitGrabbed.collider.transform.parent.gameObject.AddComponent<Rigidbody>();
         }
         else
         {
-            grabbedObject = hitGrabbed.collider.gameObject.GetComponent<Rigidbody>();
+            grabbedObject = hitGrabbed.collider.gameObject.AddComponent<Rigidbody>();
         }
 
         if (grabbedObject != null)
@@ -190,6 +221,14 @@ public class GrabSystem : MonoBehaviour
         }
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.green);
+    }
+
+    private void DoAutoThrowUpdate()
+    {
+        var PassedTime = Time.time - throwTimer;
+        if (PassedTime < AutoThrowDuration) return;
+        Throw();
+        grabbingState = GrabbingState.None;
     }
 
     private void OnDrawGizmosSelected()
@@ -205,5 +244,6 @@ public class GrabSystem : MonoBehaviour
         IsThrowing = false;
         throwMark.SetActive(false);
         player.CanMove = true;
+        grabMark.SetActive(false);
     }
 }
