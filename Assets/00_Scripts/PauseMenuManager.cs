61cee93bae92845c87ae8d21a714aaac435cee34
Ethan CHAINTRON <e.chaintron@rubika-edu.com>
2026-04-03 23:19:50 +0200

    PUTAIN DE SALOPE DE TES MORT ON A REUSSI AVEC LES INPUT MAP POUR LE MENU

--- Assets/00_Scripts/PauseMenuManager.cs
+++ Assets/00_Scripts/PauseMenuManager.cs
@@ -16,25 +16,6 @@ public class PauseMenuManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked; 
         Cursor.visible = false;
     }
-    
-    void Update()
-    {
-        if (Gamepad.current == null){return;}
-        
-        if (Gamepad.current.selectButton.wasPressedThisFrame || Gamepad.current.startButton.wasPressedThisFrame)
-        {
-            switch (currentState)
-            {
-                case MenuState.Playing:
-                    OpenPauseMenu();
-                    break;
-                
-                case MenuState.Pause:
-                    ClosePauseMenu();
-                    break;
-            }
-        }
-    }
 
     public void ClosePauseMenu()
     {
@@ -46,7 +27,7 @@ public class PauseMenuManager : MonoBehaviour
         currentState = MenuState.Playing;
     }
 
-    private void OpenPauseMenu()
+    public void OpenPauseMenu()
     {
         currentState = MenuState.Pause;
         pauseMenu.SetActive(true);
@@ -83,5 +64,19 @@ public class PauseMenuManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Return()
+    {
+        switch (currentState)
+        {
+            case MenuState.Pause:
+                ClosePauseMenu();
+                break;
+            
+            case MenuState.Settings:
+                CloseSettings();
+                break;
+        }
     }
 }
