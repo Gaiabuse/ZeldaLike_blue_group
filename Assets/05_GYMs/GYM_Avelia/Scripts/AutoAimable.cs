aa6a6ab19b44d23c91092ba8f8ec1aa39a44ce16
Bard Avelia <a.bard@rubika-edu.com>
2026-04-07 00:16:41 -0400

    fix autoaimable

--- Assets/05_GYMs/GYM_Avelia/Scripts/AutoAimable.cs
+++ Assets/05_GYMs/GYM_Avelia/Scripts/AutoAimable.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 
 public class AutoAimable : MonoBehaviour
 {
-    public float weight;
+
+    [SerializeField]
+    [Tooltip("lower weight (toward 0) means higher priority, 0 means it will always target that object")]
+    float weight = 1f;
 
 #nullable enable
     public static AutoAimable? GetNearestTargetAround(Vector3 point, float radius)
