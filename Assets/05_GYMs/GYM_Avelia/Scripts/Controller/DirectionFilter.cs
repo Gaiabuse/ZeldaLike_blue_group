b34f0720fdb69d42ea8b0863bacb41d2527a4c87
Bard Avelia <a.bard@rubika-edu.com>
2026-04-28 15:07:34 -0400

    made a test scene for the aim assist

--- Assets/05_GYMs/GYM_Avelia/Scripts/Controller/DirectionFilter.cs
+++ Assets/05_GYMs/GYM_Avelia/Scripts/Controller/DirectionFilter.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 using System;
 using System.Linq;
 
+public interface IHasProjectPoints
+{
+    Vector3 ProjectPoint(Vector2 point);
+}
+
 public class DirectionFilter : MonoBehaviour
 {
     [SerializeField]
-    private PlayerController player;
+    private IHasProjectPoints player;
 
     [SerializeField]
     [Range(0.1f, 10f)]
@@ -29,7 +34,7 @@ public class DirectionFilter : MonoBehaviour
     void Start()
     {
         // usage of Physics.SphereCast is not good too
-        Debug.LogWarning($"heavy usage of {nameof(System.Linq)} in {nameof(FilterStickInput)} it could cause some performance problem");
+        Debug.LogWarning($"heavy usage of {nameof(System.Linq)} in {nameof(FilterStickInput)} it could cause some performance problem", this);
     }
 
     public Vector3 FilterStickInput(Vector2 direction)
@@ -37,7 +42,7 @@ public class DirectionFilter : MonoBehaviour
 
         var aimableNearRaw = AutoAimable.GetTargetAround(transform.position, autoAimRadius);
 
-        if (aimableNearRaw.Count() <= 0) return player.ProjectPoint(direction);
+        if (aimableNearRaw.Count() <= 0) return player.ProjectPoint(direction) * -Mathf.Rad2Deg;
 
         var position = DeconstructIn2d(transform.position);
         var forwardDir = Vector2.right;
@@ -61,6 +66,33 @@ public class DirectionFilter : MonoBehaviour
         return player.ProjectPoint(new(MathF.Cos(finalAngle), Mathf.Sin(finalAngle)));
     }
 
+    public float FilterStickInputToAngle(Vector2 direction)
+    {
+        var aimableNearRaw = AutoAimable.GetTargetAround(transform.position, autoAimRadius);
+
+        if (aimableNearRaw.Count() <= 0) return Mathf.Atan2(direction.y, direction.x) * -Mathf.Rad2Deg;
+
+        var position = DeconstructIn2d(transform.position);
+        var forwardDir = Vector2.right;
+
+        var angleOfDir = Vector2.SignedAngle(forwardDir, direction);
+
+
+        // [WARNING] not optimal but I need to go fast or I'll never be able to test it
+        var aimableNear = aimableNearRaw
+            .Take(maxNumberOfEnemy)
+            .Select(x => DeconstructIn2d(x.transform.position))
+            .Select(x => position - x)
+            .Select(x => Vector2.SignedAngle(forwardDir, x))
+            .OrderBy(x => angleOfDir - x)
+            .First();
+
+        var finalAngle = angleOfDir + AttractTo(angleOfDir, aimableNear);
+
+        //finalAngle *= Mathf.Deg2Rad;
+
+        return finalAngle;
+    }
     // all that should be in a math helper class but tbh I'm just too lazy rn
 
     private float gaussian(float x)
