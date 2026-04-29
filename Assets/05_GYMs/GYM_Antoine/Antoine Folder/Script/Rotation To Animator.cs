765ebe8e59ad1d6fe5cf1aa220ebabd5cb004df5
DeltaUniqx <uniqxdeslandes@gmail.com>
2026-04-29 11:33:51 -0400

    Sheep Sprite Update

--- Assets/05_GYMs/GYM_Antoine/Antoine Folder/Script/Rotation To Animator.cs
+++ Assets/05_GYMs/GYM_Antoine/Antoine Folder/Script/Rotation To Animator.cs
@@ -26,12 +26,12 @@ public class RotationToAnimator : MonoBehaviour
         if (theY >= 337 || theY <= 22.5f) //0
         {
             animator.SetFloat(RotationX, 0);
-            animator.SetFloat(RotationY, -1);
+            animator.SetFloat(RotationY, 1);
         }
         if (theY >= 157.5 && theY <= 202.5f) //180
         {
             animator.SetFloat(RotationX, 0);
-            animator.SetFloat(RotationY, 1);
+            animator.SetFloat(RotationY, -1);
         }
         if (theY >= 67.5f && theY <= 112.5f) //90
         {
@@ -47,22 +47,22 @@ public class RotationToAnimator : MonoBehaviour
         if (theY > 22.5f && theY < 67.5f) //45
         {
             animator.SetFloat(RotationX, 0.75f);
-            animator.SetFloat(RotationY, -0.75f);
+            animator.SetFloat(RotationY, 0.75f);
         }
         if (theY > 112.5f && theY < 157.5) //135
         {
             animator.SetFloat(RotationX, 0.75f);
-            animator.SetFloat(RotationY, 0.75f);
+            animator.SetFloat(RotationY, -0.75f);
         }
         if (theY > 292.5f && theY < 337) //-45
         {
             animator.SetFloat(RotationX, -0.75f);
-            animator.SetFloat(RotationY, -0.75f);
+            animator.SetFloat(RotationY, 0.75f);
         }
         if (theY > 202.5f && theY < 247.5f) //-135
         {
             animator.SetFloat(RotationX, -0.75f);
-            animator.SetFloat(RotationY, 0.75f);
+            animator.SetFloat(RotationY, -0.75f);
         }
     }
 }
