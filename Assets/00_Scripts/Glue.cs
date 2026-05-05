9dd2983db841ca5455bcf4aca05835b274a15019
Ethan CHAINTRON <ethanchaintron@gmail.com>
2026-05-05 14:04:29 -0400

    moving platform

--- Assets/00_Scripts/Glue.cs
+++ Assets/00_Scripts/Glue.cs
@@ -1,9 +1,33 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Glue : MonoBehaviour
 {
+    [SerializeField] private GameObject platform;
+    [SerializeField] private Animation anim;
+    
     public void CleanGlue()
     {
+        MovePlatform();
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    private void MovePlatform()
+    {
+        anim.Play();
+        StartCoroutine(WaitForEndOfAnimation());
+    }
+
+    IEnumerator WaitForEndOfAnimation()
+    {
+        yield return new WaitForSeconds(anim.clip.length);
+        platform.tag = "Ground";
+        platform.layer = LayerMask.NameToLayer("Ground");
         Destroy(gameObject);
+        yield return null;
     }
 }
