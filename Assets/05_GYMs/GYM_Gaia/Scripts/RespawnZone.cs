d2a067a5b6a48dca58f500641824ba3bd65f54a1
Bard Avelia <a.bard@rubika-edu.com>
2026-06-03 19:31:46 -0400

    respawn button start

--- Assets/05_GYMs/GYM_Gaia/Scripts/RespawnZone.cs
+++ Assets/05_GYMs/GYM_Gaia/Scripts/RespawnZone.cs
@@ -33,6 +33,8 @@ public class RespawnZone : MonoBehaviour
         }
     }
 
+    public Vector3 GetRespawnPos() => playerRespawnPosition;
+
     void OnPLayerRespawn()
     {
         onPlayerRespawn?.Invoke();
